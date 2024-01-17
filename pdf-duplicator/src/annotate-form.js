96c0f327b33e5a1b1951649c44e05fffc8d76797
Salehuddin Loke <salehuddinloke.asriloke@etiqa.com.my>
2024-01-17 13:44:26 +0800

    UPDATE: random page annotation on page 1

--- pdf-duplicator/src/annotate-form.js
+++ pdf-duplicator/src/annotate-form.js
@@ -1,5 +1,6 @@
 import { PDFDocument, StandardFonts, rgb } from "pdf-lib";
 import { promises as fs } from "fs";
+import { faker } from '@faker-js/faker';
 
 // page functions
 import { rotatePage } from "./rotate-page.js";
@@ -21,21 +22,70 @@ export async function annotateFormPage1(pdfDoc, isMuslim) {
   try {
     // Load the existing PDF
     // const pdfDoc = await PDFDocument.load(existingPdfBytes);
+
     const form = pdfDoc.getForm()
-    const formData = {
-      policyOwner: ''
+    
+    const policyOwnerObj = await generatePersonData(true)
+    const nominee1 = await generatePersonData(true)
+    const formData =  {
+      policyNo: faker.phone.imei(),
+      policyOwner: policyOwnerObj.name + policyOwnerObj.name,
+      policyOwnerICNo: policyOwnerObj.IDNumber,
+      isNoChild: policyOwnerObj.isNotHavingChild,
+      isSingle: policyOwnerObj.isSingle,
+      nominee1_name: nominee1.name,
+      nominee1_gender: nominee1.gender,
+      nominee1_IDDesc: 'IC',
+      nominee1_IDNumber: nominee1.IDNumber
     }
 
+    // page1
+    const policyNo = form.getTextField('policyNo')
     const policyOwner = form.getTextField('policyOwner')
+    const policyOwnerICNo = form.getTextField('policyOwnerICNo')
+    policyOwner.enableMultiline()
+    const isNonMuslim = form.getCheckBox('isNonMuslim')
+    const isMuslim = form.getCheckBox('isMuslim')
+    const isSingle = form.getCheckBox('isSingle')
+    const isNoChild = form.getCheckBox('isNoChild')
+    // -----------------
 
-    policyOwner.setText('Anrsif binrty fasrmsfu')
+    // page2
+    /** NOMINEE 1 */
+    const nominee1_name = form.getTextField('nominee1_name')
+    nominee1_name.enableMultiline()
+    const nominee1_gender = form.getTextField('nominee1_gender')
+    const nominee1_IDDesc = form.getTextField('nominee1_IDDesc')
+    const nominee1_IDNumber = form.getTextField('nominee1_IDNumber')
+
+
+    const nominee2_name = form.getTextField('nominee2_name')
+    nominee2_name.enableMultiline()
+
+    const nominee2_gender = form.getTextField('nominee2_gender')
+    // ----------------
+
+
+    // SET PAGE 1
+    policyNo.setText(formData.policyNo)
+    policyOwner.setText(formData.policyOwner)
+    policyOwnerICNo.setText(formData.policyOwnerICNo)
+    isSingle.check()
+    isNoChild.check()
     if(isMuslim){
-      form.getCheckBox(`isMuslim`).check()
+      isMuslim.check()
     } else{
-      form.getCheckBox(`isMuslim`).uncheck()
+      isNonMuslim.check()
     }
-    console.log(policyOwner)
 
+    // SET PAGE 2
+    nominee1_name.setText(formData.nominee1_name)
+    nominee1_gender.setText(formData.nominee1_gender)
+    nominee1_IDDesc.setText(formData.nominee1_IDDesc)
+    nominee1_IDNumber.setText(formData.nominee1_IDNumber)
+    
+    nominee2_name.setText(formData.nominee2_name)
+    nominee2_gender.setText(formData.nominee2_gender)
 
     return form
   } catch (error) {
@@ -43,11 +93,55 @@ export async function annotateFormPage1(pdfDoc, isMuslim) {
   }
 }
 
-// const inputPath = './shared/pdf-samples/unsigned-nomination-form.pdf';
-// const signatureImagePath = './shared/signature-png/signature1_type1.png';
-// // const inputPath = '../../shared/pdf-samples/unsigned-nomination-form.pdf';
-// const outputFolder = './shared/scenario-1';
-// const count = 10;
 
-// console.log('start')
-// category1_scenario6(inputPath, outputFolder, count);
+
+export async function generatePersonData(isMale) {
+  try {
+    let name1
+    let name2
+    if(isMale){
+       name1 = faker.person.fullName( {
+        sex: 'male'
+      })
+
+       name2 = faker.person.fullName( {
+        sex: 'male'
+      })
+    }
+    else{{
+       name1 = faker.person.fullName( {
+        sex: 'female'
+      })
+
+       name2 = faker.person.fullName( {
+        sex: 'female'
+      })
+
+    }}
+    const yearRandom = faker.number.int({max:2000, min:1957})
+    const yearIDStr = `${yearRandom}`
+    const yearID = yearIDStr.slice(1,3)
+    const monthRandom = faker.number.int({max:12, min:1})
+    const monthID = monthRandom <10 ? '0'+monthRandom : monthRandom
+    const dayRandom = faker.number.int({max:28, min:1})
+    const dayID = dayRandom <10 ? '0'+dayRandom : dayRandom
+
+    const midIDNumber = faker.number.int({max:99, min:10})
+    const lastIDNumber = faker.number.int({max:9999, min:1000})
+    const id = `${yearID}${monthID}${dayID}-${midIDNumber}-${lastIDNumber}`
+
+    const personData = {
+      name: name1 + ' Bin ' + name2,
+      isMarried: false,
+      isSingle: true,
+      isNoChild: true,
+      gender: isMale ? 'male' : 'female',
+      IDNumber: id
+    }
+
+    console.log(personData)
+    return personData
+  } catch (error) {
+  }
+}
+

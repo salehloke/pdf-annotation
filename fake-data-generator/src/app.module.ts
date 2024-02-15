8cf07de34472b417b86752837c862a8c509ecf93
Salehuddin Loke <salehuddinloke.asriloke@etiqa.com.my>
2024-02-15 11:27:51 +0800

    ADDED: register new user fake data generator

--- fake-data-generator/src/app.module.ts
+++ fake-data-generator/src/app.module.ts
@@ -1,10 +1,13 @@
+import { UserFakerService } from './user/user-faker.service';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserFakerController } from './user/user-faker.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, UserFakerController],
+  providers: [
+    UserFakerService, AppService],
 })
-export class AppModule {}
+export class AppModule { }
